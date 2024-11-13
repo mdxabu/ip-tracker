0dedc836cf2623868d41e3b5ea621f71d5daf0a6
mdxabu <abdullahfakrudeen2020@gmail.com>
2024-11-13 21:31:10 +0530

    removed unwanted thing
    Signed-off-by: mdxabu <abdullahfakrudeen2020@gmail.com>

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"os"
-	"github.com/mdxabu/ipscout/web"
 	"github.com/mdxabu/ipscout/cmd"
 )
 
 func main() {
-	if len(os.Args) == 1 || os.Args[1] == "geo" {
-		go web.StartWebServer()
-	}
 	cmd.Execute()
 }
